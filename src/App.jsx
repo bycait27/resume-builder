0cb1168af7ecee8cb5d41c61565d2317b29f9c5e
Caitlin Ash <caitlinba27@gmail.com>
2024-03-03 20:16:20 -0600

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import PersonalInfo from './components/PersonalInfo'
+import Footer from './components/Footer'
 
 import './App.css'
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header />
     <PersonalInfo />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import {
+    Box,
+    Typography
+} from  '@mui/material'
+
+function Footer() {
+    return (
+        <Box>
+            <Typography>
+            Made with 🤍 by Caitlin Ash
+            </Typography>
+        </Box>
+    )
+}
+
+export default Footer
