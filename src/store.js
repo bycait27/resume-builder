30124eb4b445f8836ab7839096a5571846282daf
Caitlin Ash <caitlinba27@gmail.com>
2024-03-04 14:32:19 -0600

    Added stepperReducer to store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import stepperReducer from './features/stepper/stepperSlice'
 
 export const store = configureStore({
     reducer: {
-
+        stepper: stepperReducer,
     },
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
