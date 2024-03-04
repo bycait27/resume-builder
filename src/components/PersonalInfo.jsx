5fed0d7c482c7e877cff9d951d02c2443a2780b4
Caitlin Ash <caitlinba27@gmail.com>
2024-03-03 20:14:20 -0600

    Added form for personal info section of resume

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,14 +1,56 @@
 import {
     Box,
-    Typography
+    Typography,
+    Button,
+    TextField,
+    Card
 } from  '@mui/material'
 
 function PersonalInfo() {
     return (
         <Box>
-            <Typography>
-
-            </Typography>
+            <Card>
+                {/* user's name */}
+                <Typography>
+                    Your Name:
+                </Typography>
+                <TextField></TextField>
+                {/* user's location */}
+                <Typography>
+                    Your Location:
+                </Typography>
+                <TextField
+                helperText={'City, State'}
+                ></TextField>
+                {/* user's phone number */}
+                <Typography>
+                    Phone Number:
+                </Typography>
+                <TextField></TextField>
+                {/* user's email */}
+                <Typography>
+                    Email:
+                </Typography>
+                <TextField></TextField>
+                {/* user's portfolio link */}
+                <Typography>
+                    Portfolio Link:
+                </Typography>
+                <TextField></TextField>
+                {/* user's GitHub link */}
+                <Typography>
+                    GitHub Link:
+                </Typography>
+                <TextField></TextField>
+                {/* user's LinkedIn link */}
+                <Typography>
+                    LinkedIn Link:
+                </Typography>
+                <TextField></TextField>
+                <Button>
+                    Submit Info
+                </Button>
+            </Card>
         </Box>
     )
 }
